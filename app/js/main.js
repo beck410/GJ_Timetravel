df1d989799a135c2b6922777e45431b17faddf1b
brandontate <blt900@gmail.com>
2015-01-24 13:36:57 -0600

    Added interval functions for arrow

--- app/js/main.js
+++ app/js/main.js
@@ -3,10 +3,22 @@ angular.module("TTT",["ng-route"])
 game = (function(){
   'use strict'
 
-  var levels = ["emu", "van"];
+  var levels = ["emu", "van", "dinosaur"];
 
   function randomizer() {
     return Math.floor(Math.random * levels.length);
+  }
+
+  function arrowInterval() {
+    setInterval(arrowFlash, 600);
+  }
+
+  function arrowFlash() {
+    $(".arrow").toggle;
+  }
+
+  function clearArrowInterval(function) {
+    clearInterval(function);
   }
 
   return;
