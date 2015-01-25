df152bd0370186ce4082c6a7337b5225bb044e31
Alex Story <astory11@yahoo.com>
2015-01-25 11:52:05 -0600

    added dialogue to dint page

--- app/js/controllers/dinosaur.controller.js
+++ app/js/controllers/dinosaur.controller.js
@@ -1,20 +1,22 @@
 ;(function(){
-  'use strict'; 
+  'use strict';
   angular.module('TTT')
   .controller('dinosaurController',function($location,$scope,dinoAnimate){
     var vm = this;
+    vm.option = true;
 
-    vm.doorChoice = function(){ 
+    $('.textbox').text('You arrive inat the next era, looking out of the window you see dinosaurs roaming about what do you do?');
+
+
+    vm.doorChoice = function(){
       dinoAnimate.die();
     };
 
     vm.leverChoice = function(){
-      dinoAnimate.nextLevel(function(lever){ 
+      dinoAnimate.nextLevel(function(lever){
         $location.path('/' + game.randomizer());
         $scope.$apply();
-      }); 
+      });
     };
   });
 })();
-
-
